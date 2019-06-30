1af21d535bfd062e6b86e1c121a9f3c27db8a141
krisbebb <krisbebb@gmail.com>
2019-06-30 11:53:34 +1000

    Init commit

---
+++ src/playground/es6-arrow-function.js
@@ -0,0 +1,29 @@
+// arguments
+
+const add = (a, b) => {
+  // console.log(arguments)
+  return a + b
+}
+console.log(add(55, 1, 1000))
+
+
+// this keyword
+
+const user = {
+  name: 'Andrew',
+  cities: ['Philadelphia', 'New York', 'Dublin'],
+  printPlacesLived() {
+    return this.cities.map((city) => this.name + ' has lived in ' + city)
+  }
+}
+console.log(user.printPlacesLived())
+
+const multiplier = {
+  numbers: [1, 2, 3],
+  multiplyBy: 2,
+  multiply() {
+    return this.numbers.map((number) => number * this.multiplyBy)
+  }
+}
+
+console.log(multiplier.multiply()) // [1, 2, 3] 2 [2, 4, 6]
